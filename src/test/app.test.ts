6e7a297f4fe52b4b0aee73414aead81088c3afab
Seungmin Ha <seungmin.ha@ggq.gg>
2022-04-19 08:56:15 +0900

    update LoggerImp [TEST-36]

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -9,7 +9,7 @@ interface Logger {
 
 class LoggerImp implements Logger {
   constructor() {
-    console.log("constructor is called2");
+    console.log("constructor is called3");
   }
 
   info(message: string): void {
